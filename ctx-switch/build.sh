ae06c5fc2c6650fa31bfa3cae5bcb356994ef20b
Daisuke Sugiyama <dsugiyama@outlook.jp>
2017-01-12 22:18:03 +0900

    fix

--- ctx-switch/build.sh
+++ ctx-switch/build.sh
@@ -1,4 +1,4 @@
-if [[$1 == knl ]]; then
+if [[ $1 == knl ]]; then
     ICC="icc -std=gnu11 -O3 -ipo  -xMIC-AVX512"
     GCC="gcc -std=gnu11 -O3 -flto -mavx512f -mavx512cd -mavx512er -mavx512pf"
     OMPC_FLAGS="/home/sugiyama/inst/omni-compiler_cilk/opt/lib/ompc_main.o \
