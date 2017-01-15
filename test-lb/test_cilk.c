fa4b3e50614cda11998dcea3d70f15d2c23cdeec
Daisuke Sugiyama <dsugiyama@outlook.jp>
2017-01-15 23:09:40 +0900

    [test-lb] add suburi

--- test-lb/test_cilk.c
+++ test-lb/test_cilk.c
@@ -12,6 +12,13 @@ int main(int argc, char *argv[])
 {
     delay_length = delay_length_from_sec(0.001);
 
+    #pragma cilk grainsize = 1
+    cilk_for (int i = 0; i < 256; i++) {
+        for (int j = 0; j < 100; j++) {
+            delay(delay_length);
+        }
+    }
+
     double loop_start = get_clock();
     #pragma cilk grainsize = 1
     cilk_for (int i = 0; i < 256; i++) {
