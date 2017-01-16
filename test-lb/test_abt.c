ad24342a7fe7438e258f11709a2c91b6c47f5ab0
Daisuke Sugiyama <dsugiyama@outlook.jp>
2017-01-16 13:54:39 +0900

    [test-lb] join overhead

--- test-lb/test_abt.c
+++ test-lb/test_abt.c
@@ -7,6 +7,7 @@ void delay(int delay_length);
 
 int delay_length;
 double iter_start[256];
+double iter_end[256];
 
 void _loop_func_0(uint64_t from, uint64_t to_exclusive, int step, void *args[])
 {
@@ -14,6 +15,7 @@ void _loop_func_0(uint64_t from, uint64_t to_exclusive, int step, void *args[])
     for (int i = 0; i < 100; i++) {
         delay(delay_length);
     }
+    iter_end[from] = get_clock();
 }
 
 int ompc_main(int argc, char *argv[])
@@ -22,9 +24,10 @@ int ompc_main(int argc, char *argv[])
 
     double loop_start = get_clock();
     ompc_loop_divide_conquer(_loop_func_0, 0, NULL, 0, 256, 1, 256);
+    double loop_end = get_clock();
 
     for (int i = 0; i < 256; i++) {
-        printf("%e\n", iter_start[i] - loop_start);
+        printf("%e %e\n", iter_start[i] - loop_start, loop_end - iter_end[i]);
     }
 
     return 0;
